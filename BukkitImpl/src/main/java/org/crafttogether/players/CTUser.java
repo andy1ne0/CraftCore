9d799516620556aa7b60f1ab3d418137b6add99f
Andrew <andy1ne0@gmail.com>
2016-05-13 17:09:50 +1000

    Removed my default copyright headers, another small Codacy fix..

--- BukkitImpl/src/main/java/org/crafttogether/players/CTUser.java
+++ BukkitImpl/src/main/java/org/crafttogether/players/CTUser.java
@@ -10,7 +10,11 @@ import org.crafttogether.user.Punishment;
 import org.crafttogether.user.User;
 
 import java.lang.ref.WeakReference;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
--- BungeeImpl/src/main/java/org/crafttogether/bungee/locale/LocaleManager.java
+++ BungeeImpl/src/main/java/org/crafttogether/bungee/locale/LocaleManager.java
@@ -12,22 +12,8 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * All contents of this file, except where specified elsewhere, is the copyrighted property of Andrew Petersen.
- * Any works derived from this must only be done so if permission is given beforehand by the author (Andrew Petersen).
- * Failure to receive proper permission to use the works provided will result in termination of any agreements between the third party and Andrew Petersen.
- * If permission to use the contents of this file are granted to you, the client party, you must not take credit and/or claim these works as your own.
- * Doing so will void any contracts, or permission given, by Andrew Petersen, unless notified otherwise.
- * This file is provided as-is, with absolutely no warranty provided. By using this file, you understand this
- * - regardless of if you have been warned or not prior to using this file, any liabilities or harm caused by this file is your problem.
- * If this file is available for public download, by intention of the author, derirative works without the consent of Andrew Petersen are allowed.
- * However, once again, you must give credit to the author of the code.
- * If this has been downloaded or distributed without the permission of the author, all permissions granted above are voided.
- * It is also required that in these circumstances, you cease to use the file/contents.
- * Last edited: 13/05/2016 (dd/mm/yy), 07:26
- * Package Name: org.crafttogether.bungee.locale
- * Project Name: CraftCore
- *
  * @author Andrew Petersen
+ * @version 0.1
  */
 public class LocaleManager implements ILocaleManager {
 
